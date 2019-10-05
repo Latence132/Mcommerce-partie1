fa1ac63e6dd61ad2e4f8231640b27272760f8b91
Latence132 <alexandre.chaumet@gadz.org>
2019-10-05 22:45:48 +0200

    Partie 4 - PostMAn

---
+++ src/main/java/com/ecommerce/microcommerce/web/controller/DataResponse.java
@@ -0,0 +1,38 @@
+package com.ecommerce.microcommerce.web.controller;
+
+import java.util.List;
+
+import com.ecommerce.microcommerce.model.Product;
+
+public class DataResponse {
+	
+	private Boolean success;
+    private Integer count;
+    private List<?> data;
+   
+    public void setSuccess(Boolean bool) {
+    	this.success = bool;
+    }
+    
+    public Boolean getSuccess() {
+    	return this.success;
+    }
+    
+    public void setCount(Integer count) {
+    	this.count = count;
+    }
+    
+    public Integer getCount() {
+    	return this.count;
+    }
+    
+    public void setProducts(List<?> data) {
+    	this.data = data;
+    }
+    
+    public List<?> getProduct() {
+    	return this.data;
+    }
+
+
+}
